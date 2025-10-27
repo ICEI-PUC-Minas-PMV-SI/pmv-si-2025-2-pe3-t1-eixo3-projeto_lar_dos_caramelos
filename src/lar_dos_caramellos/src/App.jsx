c67a6d9b15efad629399180953f8a0cff27871c0
calazans17 <paaocalazans@sga.pucminas.br>
2025-10-27 18:14:09 -0300

    pagina de dicas Pets

--- src/lar_dos_caramellos/src/App.jsx
+++ src/lar_dos_caramellos/src/App.jsx
@@ -1,51 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Link } from 'react-router-dom';
-import { Routes, Route } from 'react-router-dom';
-import Login from './pages/loginPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import LoginPage from './pages/loginPage';
 import RegisterPage from './pages/registerPage';
 import CardRegisterPage from './pages/CardRegisterPage';
-import Navbar from './components/NavbarComponent.jsx';
-import HomePage from './pages/HomePage.jsx';
-import Eventos from './pages/Eventos.jsx';
+import NavbarComponent from './components/NavbarComponent';
+import HomePage from './pages/HomePage';
+import EventosPage from './pages/Eventos';
+import DicasPetsPage from './pages/DicasPetsPage';
 
 function App() {
-
-
   return (
     <>
-{/* Global styles should be placed in App.css or index.css instead of inline <style> in JSX */}
-
-
       <BrowserRouter>
-      <Navbar/> 
+        <NavbarComponent />
 
-
-         
-      
-
-
-      <Routes>
-        <Route path='/' element={<HomePage/>} />
-
-        <Route path='/login' element={<Login/>} />
-
-        <Route path ='/register' element= {<RegisterPage/>} />
-
-      <Route path ='/regCard' element= {<CardRegisterPage/>} />
-      <Route path='/eventos' element={<Eventos />} />
-      </Routes>
-
-      
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/register' element={<RegisterPage />} />
+          <Route path='/register-card' element={<CardRegisterPage />} />
+          <Route path='/eventos' element={<EventosPage />} />
+          <Route path='/dicas' element={<DicasPetsPage />} />
+        </Routes>
       </BrowserRouter>
-
-
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
