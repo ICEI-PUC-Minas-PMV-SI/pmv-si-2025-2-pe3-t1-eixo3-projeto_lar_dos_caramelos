07e33eb38b0969b5923943c6bfc5141b10a01f0d
Raquel Ribeiro <raquel.ribeiro.600492@sga.pucminas.br>
2025-11-11 14:00:25 -0300

    Alterar nome

--- src/lar_dos_caramellos/src/pages/registerPage.jsx
+++ src/lar_dos_caramellos/src/pages/registerPage.jsx
@@ -90,7 +90,7 @@ const Register = () => {
         <div className="text-center mb-4">
           <Link to="/" className="d-inline-flex align-items-center gap-2 text-decoration-none">
             <Heart size={36} color="orange" fill="orange" />
-            <span className="fs-3 fw-bold text-dark">Amigo de Pata</span>
+            <span className="fs-3 fw-bold text-dark">Lar dos Caramellos</span>
           </Link>
         </div>
 
